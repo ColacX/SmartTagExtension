ecc64740cdb6bb24fb2c2290ebd1800f28777eed
ColacX <DanielZheng1989@gmail.com>
2015-09-06 15:04:09 +0200

    fixed the tag editor

--- SmartTagExtension/popup.js
+++ SmartTagExtension/popup.js
@@ -37,57 +37,77 @@ angular.module('MyModule').directive('myAutoFocus', ['$document', function ($doc
 angular.module('MyModule').directive('myTagEditor', ['$document', '$log', '$compile', function ($document, $log, $compile) {
 	return {
 		restrict: "E",
-		template: "<span class='tag-input' contenteditable='true' ng-keypress='keyPress($event)'>default html</span>",
+		template: "<span></span><span class='tag-input' contenteditable='true' ng-keypress='stuff.keyPress($event)'></span>",
 		controller: function ($scope) {
-			var tags, self;
-			tags = [];
-			self = this;
+			var tags, ctrl;
+			tags = {};
+			ctrl = this;
 
-			$scope.keyPress = function ($event) {
-				if ($event.keyCode === 32) {
-					//space key was pressed
+			$scope.stuff = {
+				keyPress: function ($event) {
+					if ($event.keyCode === 32) {
+						//space key was pressed
 
-					if (!self.tagInputElement || typeof self.tagInputElement.innerHTML != "string") {
-						//return if tagInputElement is bad
+						if (!ctrl.tagInputElement || typeof ctrl.tagInputElement.innerHTML != "string") {
+							//return if tagInputElement is bad
+							return;
+						}
+
+						//convert html-code to plain-text then split to tagNames
+						var textData, tagNames;
+						textData = angular.element("<textarea />").html(ctrl.tagInputElement.innerHTML).text();
+						textData = textData.replace(/<br>/g, " ");
+						tagNames = textData.match(/\S+/g);
+
+						if (tagNames) {
+							$scope.stuff.append(tagNames.shift());
+							ctrl.tagInputElement.innerHTML = tagNames.join(" ");
+						}
+					}
+					else if ($event.keyCode === 32) {
+						//enter key was pressed
+						//todo add save
+					}
+				},
+				append: function (tagName) {
+					if (!tagName || tags[tagName]) {
+						//todo possible show some indication
+						//return if name is bad or tag already exists
 						return;
 					}
 
-					//convert html-code to plain-text then split to tagNames
-					var textData, tagNames;
-					textData = angular.element("<textarea />").html(self.tagInputElement.innerHTML).text();
-					textData = textData.replace(/<br>/g, " ");
-					tagNames = textData.match(/\S+/g);
+					//create tag item
+					var childScope = $scope.$new();
+					childScope.tagName = tagName;
+					var tagItemElement = $compile("<my-tag-item></my-tag-item>")(childScope);
 
-					if (tagNames) {
-						$log.debug(tagNames);
-						$log.debug(tagNames.shift());
-						self.tagInputElement.innerHTML = tagNames.join(" ");
+					//save child element so it can be deleted later
+					tags[tagName] = {
+						element: tagItemElement,
+						scope: childScope
+					}; 
+
+					$log.debug(ctrl.tagContainerElement);
+					angular.element(ctrl.tagContainerElement).append(tagItemElement);
+				},
+				remove: function (tagName) {
+					if (tags[tagName]) {
+						tags[tagName].element.remove();
+						delete tags[tagName];
+						$log.debug(tags);
 					}
 				}
-				else if ($event.keyCode === 32) {
-					//enter key was pressed
-					
-				}
-			}
-
-
-			this.append = function (tagName) {
-				$log.debug("append");
-				$log.debug(tagName);
-			}
-
-			this.remove = function (tagName) {
-				$log.debug("remove");
-				$log.debug(tagName);
-			}
+			};
 		},
 		link: function ($scope, $element, $attr, controller) {
 			$log.debug($element);
 			$element.addClass('tag-container');
-			controller.tagInputElement = $element[0].children[0];
-
+			controller.tagContainerElement = $element[0].childNodes[0];
+			controller.tagInputElement = $element[0].children[1];
+			controller.tagEditorElement = $element;
 
 			angular.element($document).ready(function () {
+				controller.tagInputElement.focus();
 			});
 
 			$compile($element.contents())($scope);
@@ -95,16 +115,12 @@ angular.module('MyModule').directive('myTagEditor', ['$document', '$log', '$comp
 	};
 }]);
 
-angular.module('MyModule').directive('myTagItem', ['$document', '$log', function ($document, $log) {
+angular.module('MyModule').directive('myTagItem', ['$document', '$log', '$compile', function ($document, $log, $compile) {
 	return {
 		restrict: "E",
-		template: "<span class='tag-text'>{{tagName}}</span><span class='icon-button icon-delete' ng-click='remove()'></span>",
-		require: "^myTagEditor",
-		link: function (scope, element, attributes, controller) {
-			$log.debug(element);
-			element.addClass('tag-item');
-
-			controller.test();
+		template: "<span class='tag-text'>{{tagName}}</span><span class='icon-button icon-delete' ng-click='stuff.remove(tagName)'></span>",
+		link: function ($scope, $element, $attr, controller) {
+			$element.addClass("tag-item");
 		}
 	};
 }]);
