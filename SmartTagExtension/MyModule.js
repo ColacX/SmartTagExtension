4b59df4b890968eb5b939919dd388c56d2043c54
ColacX <DanielZheng1989@gmail.com>
2016-03-04 14:08:01 +0100

    removed unused libraries

--- SmartTagExtension/MyModule.js
+++ SmartTagExtension/MyModule.js
@@ -1,30 +1,7 @@
 'use strict';
 
 //define module and dependencies to other modules
-angular.module('MyModule', ['pascalprecht.translate', 'ngAnimate', 'ui.bootstrap']);
-
-//define translations
-angular.module('MyModule').config(function ($translateProvider) {
-
-	$translateProvider.translations('en', {
-		HEADLINE: 'Hello there, This is my awesome app!',
-		INTRO_TEXT: 'And it has i18n support!'
-	});
-
-	$translateProvider.translations('de', {
-		HEADLINE: 'Hey, das ist meine großartige App!',
-		INTRO_TEXT: 'Und sie untersützt mehrere Sprachen!'
-	});
-
-	$translateProvider.translations('sv', {
-		HEADLINE: 'Hejsan detta är min awesome app!',
-		INTRO_TEXT: 'Und sie untersützt mehrere Sprachen!'
-	});
-
-	//$translateProvider.preferredLanguage('de');
-	//$translateProvider.preferredLanguage('en');
-	$translateProvider.preferredLanguage('sv');
-});
+angular.module('MyModule', ['ui.bootstrap']);
 
 //define directives
 angular.module('MyModule').directive('myAutoFocus', ['$document', '$timeout', function ($document, $timeout) {
